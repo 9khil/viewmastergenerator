ce8442b3ceb0391e54064c52d9ab71ecb878bde6
Nikhil André Luthra <nikhil.a.luthra@gmail.com>
2024-09-17 08:43:09 +0200

    figuring out reel display and file upload

--- src/components/Reel/Reel.tsx
+++ src/components/Reel/Reel.tsx
@@ -6,9 +6,20 @@ import { useViewMasterStore } from "../../store";
 function Reel() {
   const { images } = useViewMasterStore();
 
+  const renderImages = () => {
+    return images.map((image: string, index: number) => {
+      return <img className={`reel-${index + 1}`} src={image} />;
+    });
+  };
+
   return (
     <div className={styles.reelContainer}>
-      <img src={reelUrl} />
+      <div
+        style={{ backgroundImage: `url(${reelUrl})` }}
+        className={styles.reel}
+      >
+        <div className={styles.reelImagesContainer}>{renderImages()}</div>
+      </div>
     </div>
   );
 }
